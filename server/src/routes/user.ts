5c6d658c180d4733e422c54e3c332083c315f1b5
Noé <noe.jais@epitech.eu>
2023-11-19 19:23:43 +0100

    [ADD] Middleware: isUserExist and accessToken provided

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,13 +1,15 @@
 import express from 'express';
+import accessToken from '../middleware/accessToken';
+import isUserExist from '../middleware/isUserExist';
 
-import { getMe, patchMe } from '../api/users';
+import { getMe, patchMe } from '../api/user';
 const router = express.Router();
 
-router.get('/me', (req, res) => {
+router.get('/me', accessToken, isUserExist, (req, res) => {
     getMe(req, res)
 })
 
-router.patch('/me', (req, res) => {
+router.patch('/me', accessToken, isUserExist, (req, res) => {
     patchMe(req, res)
 })
 
