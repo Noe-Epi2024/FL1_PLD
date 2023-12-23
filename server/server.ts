6c422d41d6314629035770ccf87bed737fdcf33c
Noé <noe.jais@epitech.eu>
2023-12-23 19:58:17 +0100

    [ADD] picture post and get request

--- server/server.ts
+++ server/server.ts
@@ -13,11 +13,13 @@ mongo().catch(console.error);
 import auth from './src/routes/auth';
 import user from './src/routes/user';
 import project from './src/routes/project';
+import picture from './src/routes/picture';
 
 // Use Routes
 app.use("/", auth);
 app.use("/user", user);
 app.use("/project", project);
+app.use("/picture", picture);
 
 const port = process.env.PORT || 8080;
 
---
+++ server/src/api/picture/index.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from 'express';
+import crypto from 'crypto';
+import { uploadFileToS3, getSignedUrlFromS3, s3BucketName } from '../../database/s3';
+
+export async function addPicture(req: Request, res: Response) {
+    try {
+        const name = crypto.randomBytes(32).toString('hex');
+
+        if (!req.file) {
+            return res.status(400).send('No file uploaded.');
+        }
+
+        await uploadFileToS3(req.file.buffer, name, req.file.mimetype);
+
+        const url = await getSignedUrlFromS3(name);
+        res.status(200).send({ url, name });
+    } catch (error) {
+        console.error("Error adding picture:", error);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+export async function getPicture(req: Request, res: Response) {
+    try {
+        const name = req.query.name as string;
+
+        if (!name) {
+            return res.status(400).send('Missing file name.');
+        }
+
+        const url = await getSignedUrlFromS3(name);
+        res.status(200).send({ url, name });
+    } catch (error) {
+        console.error("Error getting picture:", error);
+        res.status(500).send('Internal Server Error');
+    }
+}
