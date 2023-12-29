5b82d5b3f5dc9f82a1b6f73bb6f42063a944b9ac
Noé <noe.jais@epitech.eu>
2023-12-29 15:57:25 +0100

    [REMOVE] server folder from repository

--- server/src/types/members.ts
+++
@@ -1,7 +0,0 @@
-import { Types } from "mongoose";
-
-export interface Member {
-    userId: Types.ObjectId;
-    role: string;
-    name: string;
-}
--- server/src/types/subtasks.ts
+++
@@ -1,7 +0,0 @@
-import { Types } from 'mongoose';
-
-export interface Subtask {
-    name: string;
-    isDone: boolean;
-    _id: Types.ObjectId;
-}
